6fa6be745f63dde4a952d4aa57b5eece243c2644
Umeru91234 <umeru4345@gmail.com>
2024-10-24 11:50:16 +0500

    Hello World

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      {/* <div className='pl-[30%] text-[36px] my-[10%] h-[400px] bg-[url('/images/umer.JFIF') m-[10px] mx-[20px] bg-[#9aa73c] '>Hello Umer</div> */}
+     <div>
+         <h1 className='text-3xl text-extrabold font-black italic'> <span className='text -9xl bg-Umer'>World</span>Hello I'm writing Tailwind CSS</h1>
+         
+
+     </div>
+    </div>
+  )
+}
+
+export default page
