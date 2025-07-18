696b636b682d3be86c50665ddd4f45c517aa10de
Umeru91234 <umeru4345@gmail.com>
2025-07-19 00:10:24 +0500

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      {/* <div className='pl-[30%] text-[36px] my-[10%] h-[400px] bg-[url('/images/umer.JFIF') m-[10px] mx-[20px] bg-[#9aa73c] '>Hello Umer</div> */}
+      {/* <div className='pl-[30%] text-[36px] my-[10%] h-[400px] bg-[url('/images/umer.JFIF') hn m-[10px] mx-[20px] bg-[#9aa73c] '>Hello Umer</div> */}
      <div>
          <h1 className='text-3xl text-extrabold font-black italic'> <span className='text -9xl bg-Umer'>World</span>Hello I'm writing Tailwind CSS</h1>
          
